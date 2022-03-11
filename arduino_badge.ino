5abc4b1d4fb6f995242f93968b4cf797d9b7a6df
Florian04 <77146047+Florian04@users.noreply.github.com>
2022-03-11 12:30:32 +0100

    Add files via upload

--- arduino_badge.ino
+++ arduino_badge.ino
@@ -7,29 +7,29 @@ PN532_HSU interface(Serial1); //Connexion du module Grove NFC sur le serial 1
 NfcAdapter nfc = NfcAdapter(interface);
 
 String UID_scan;
+
 /**************************************************************************************************/
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("NDEF Reader");
-    nfc.begin(); //Initialisation du module
+  Serial.begin(9600);
+  Serial.println("NDEF Reader");
+  nfc.begin(); //Initialisation du module
 }
 
 void loop()
 {
-    if (nfc.tagPresent())
-    {
-        NfcTag tag = nfc.read(); //Lecture de la carte NFC
-        UID_scan = tag.getUidString(); //Acquisition du code UID de la carte
-        Serial.println("ID Card : " + UID_scan); //Renvoi sur le moniteur du code UID
-
-        //TO DO
-        //Vérifier que les horaires de badge sont corrects (8h30 - 23h)
-        //Envoyer une requête à l'API avec l'ID de la carte et du cadenas
-        
-
-        
-    }
-    delay(2000);
+  if (nfc.tagPresent())
+  {
+    NfcTag tag = nfc.read(); //Lecture de la carte NFC
+    UID_scan = tag.getUidString(); //Acquisition du code UID de la carte
+    Serial.println("ID Card : " + UID_scan); //Renvoi sur le moniteur du code UID
+    
+    digitalWrite(2, HIGH);
+    delay(500);             
+    digitalWrite(2, LOW);    
+    delay(500);   
+    
+  }
+  delay(2000);
 }
