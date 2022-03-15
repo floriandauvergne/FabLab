8731545e35788a6deae0796b9d472baf9babb579
Florian04 <77146047+Florian04@users.noreply.github.com>
2022-03-15 12:23:54 +0100

    Add files via upload

--- arduino_badge.ino
+++ arduino_badge.ino
@@ -7,6 +7,7 @@ PN532_HSU interface(Serial1); //Connexion du module Grove NFC sur le serial 1
 NfcAdapter nfc = NfcAdapter(interface);
 
 String UID_scan;
+int LED = 2;
 
 /**************************************************************************************************/
 
@@ -25,11 +26,26 @@ void loop()
     UID_scan = tag.getUidString(); //Acquisition du code UID de la carte
     Serial.println("ID Card : " + UID_scan); //Renvoi sur le moniteur du code UID
     
-    digitalWrite(2, HIGH);
-    delay(500);             
-    digitalWrite(2, LOW);    
-    delay(500);   
-    
+    blink_LED();
+    verif_autorisation();
+
   }
-  delay(2000);
+
+}
+
+void blink_LED()
+{
+    digitalWrite(LED, HIGH);
+    delay(500);             
+    digitalWrite(LED, LOW);    
+    delay(500);  
+}
+
+void verif_autorisation()
+{
+  //Faire une requete pour l'API pour verifier l'horaire et si la personne à déjà badger
+  //récupérer le JSON de la requete et le traiter
+
+  //Si les informations correspondent : ouvrir le cadenas et (opt)emettre un son
+  //Sinon : (opt)emettre un son
 }
