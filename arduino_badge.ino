bba869b90a1e35dafe6395b79ba63902ceb39085
Florian04 <77146047+Florian04@users.noreply.github.com>
2022-03-18 12:28:00 +0100

    Add files via upload

--- arduino_badge.ino
+++ arduino_badge.ino
@@ -1,51 +1,62 @@
+#include <Bridge.h>
+#include <BridgeClient.h>
+#include <b64.h>
+#include <HttpClient.h>
 
-#include "PN532_HSU.h"
-#include "PN532.h"
-#include "NfcAdapter.h"
+
+#include "ArduinoJson.h"  //Librairie de gestion des JSON
+#include "PN532_HSU.h"    //Librairie de lecture du capteur NFC
+#include "PN532.h"        //Librairie de lecture du capteur NFC
+#include "NfcAdapter.h"   //Librairie du capteur NFC
+
 
 PN532_HSU interface(Serial1); //Connexion du module Grove NFC sur le serial 1
 NfcAdapter nfc = NfcAdapter(interface);
 
 String UID_scan;
-int LED = 2;
-
-/**************************************************************************************************/
+int LED = 13;
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("NDEF Reader");
   nfc.begin(); //Initialisation du module
+  Serial.println("NDEF Reader");
+  
+  DynamicJsonDocument doc(2048);
+
+  Bridge.begin();
+
 }
 
 void loop()
 {
+
   if (nfc.tagPresent())
-  {
-    NfcTag tag = nfc.read(); //Lecture de la carte NFC
-    UID_scan = tag.getUidString(); //Acquisition du code UID de la carte
-    Serial.println("ID Card : " + UID_scan); //Renvoi sur le moniteur du code UID
-    
-    blink_LED();
-    verif_autorisation();
+  { 
+//    NfcTag tag = nfc.read(); //Lecture de la carte NFC
+//    UID_scan = tag.getUidString(); //Acquisition du code UID de la carte
+//    UID_scan.replace(" ", "");
+//    
+//    Serial.println("ID Card : " + UID_scan); //Renvoi sur le moniteur du code UID
 
+    String url = "https://a3b49ec7-c30d-4962-9bad-63b0c319f410.mock.pstmn.io/retour";
+    //String url = "http://51.210.151.13/badge.php?type=open&idCarte=" + UID_scan + "&idCadenas=0";
+    Serial.println(url);
+    delay(1000);
+    requete_deverouiller();
   }
-
 }
 
-void blink_LED()
+void requete_deverouiller()
 {
-    digitalWrite(LED, HIGH);
-    delay(500);             
-    digitalWrite(LED, LOW);    
-    delay(500);  
-}
-
-void verif_autorisation()
-{
-  //Faire une requete pour l'API pour verifier l'horaire et si la personne à déjà badger
-  //récupérer le JSON de la requete et le traiter
-
-  //Si les informations correspondent : ouvrir le cadenas et (opt)emettre un son
-  //Sinon : (opt)emettre un son
+  HttpClient client;
+  String url = "https://a3b49ec7-c30d-4962-9bad-63b0c319f410.mock.pstmn.io/retour";
+  client.get(url);
+  
+  while(client.available())
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+  delay(5000);
 }
