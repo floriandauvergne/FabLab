141494f98a6a83a79ac8800ec197c66cad273e94
floriandauvergne <77146198+floriandauvergne@users.noreply.github.com>
2022-04-08 08:42:14 +0200

    Delete FabLab_V1 directory

--- FabLab_V1/script/Deconnexion.js
+++
@@ -1,6 +0,0 @@
-export function deconnexion_principale(){
-    $("#Deconnexion").click(function (){
-        // if(window.confirm("Voulez-vous vous deconnecter ?"))
-            location.reload();
-    })
-}
