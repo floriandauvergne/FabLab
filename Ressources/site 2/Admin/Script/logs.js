5063bf80fa5db9efa310cf8824b68d9ab5c08427
floriandauvergne <77146198+floriandauvergne@users.noreply.github.com>
2022-04-24 19:57:37 +0200

    Add files via upload

---
+++ Ressources/site 2/Admin/Script/logs.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+    var request = $.ajax({
+        method: "GET",
+        url: "../../api/log/recuperer.php",
+        data: {brut:1},
+        dataType: "json"
+    });
+
+    request.done(function (msg){
+        for(let i=(msg.length-1);i>=0;i--){
+            $("#logs").append("<ul>"+msg[i].Nom+" a ouvert "+msg[i].NomCadenas+" le "+msg[i].Date+" à "+msg[i].Horaire+"</ul>")
+            console.log(msg[i].Jour)
+        }
+        console.log(msg)
+
+    })
+    request.fail(function (jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
+})
